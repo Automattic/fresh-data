b6633d88391f1254a785d00e4c471fd66dd94ec0
Kevin Killingsworth <kevin@redfenix.com>
2018-03-24 18:00:35 -0400

    Fix eslint for example webpack config
    This fixes some eslint errors in the webpack file

--- examples/hello-world/webpack.config.js
+++ examples/hello-world/webpack.config.js
@@ -1,5 +1,4 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require( 'path' );
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
 
 module.exports = {
 	module: {
