629c4ef7e795e727247e9b43b8dcb0a29061a420
Kevin Killingsworth <kevin@redfenix.com>
2018-04-06 23:35:58 -0500

    Use create-react-app for hello-world example.
    This updates the hello-world example skeleton to use create-react-app.

---
+++ examples/hello-world/README.md
@@ -0,0 +1,11 @@
+# Fresh Data - Hello World
+
+This is the simplest example possible using Fresh Data. It adds a locally implemented API to fetch data to be presented in a React component.
+
+This example project was built with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+## Try it out!
+--------------
+Simply run:
+
+```npm start```
---
+++ examples/hello-world/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Fresh Data</h1>
+				</header>
+				<p className="App-intro">
+					Hello World!
+				</p>
+			</div>
+		);
+	}
+}
+
+export default App;
