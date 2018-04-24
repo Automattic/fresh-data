70c8547f0ee9b932f44744fb0479a74139cdb550
Kevin Killingsworth <kevin@redfenix.com>
2018-04-24 14:59:28 -0500

    Change requirements to component-based mapping.
    This is the first set of changes to move the primary method of gathering
    requirements to a component-based approach which accounts for components
    mounting and unmounting. It also switches the endpoints to a tree
    structure to allow for nested endpoints. It also accounts for queries
    for a given endpoint and handling requirements for those separately.

--- src/client/__tests__/requirements.spec.js
+++ src/client/__tests__/requirements.spec.js
@@ -1,95 +1,213 @@
 import { SECOND } from '../../utils/constants';
 import {
 	DEFAULTS,
-	reduceEndpointRequirements,
-	reduceItemRequirements,
+	addRequirementParams,
+	addEndpointRequirement,
+	combineComponentRequirements,
 } from '../requirements';
 
-describe( 'reduceItemRequirements', () => {
-	it( 'should not update when nothing has changed', () => {
-		const reqs = reduceItemRequirements( DEFAULTS, { ...DEFAULTS } );
-		expect( reqs ).toBe( DEFAULTS );
+describe( 'addRequirementParams', () => {
+	it( 'should set defaults if not given.', () => {
+		const endpointRequirements = {};
+		addRequirementParams( endpointRequirements, {} );
+		expect( endpointRequirements.freshness ).toEqual( DEFAULTS.freshness );
+		expect( endpointRequirements.timeout ).toEqual( DEFAULTS.timeout );
+	} );
+	it( 'should set freshness if not previously set.', () => {
+		const endpointRequirements = {};
+		addRequirementParams( endpointRequirements, { freshness: 30 * SECOND } );
+		expect( endpointRequirements.freshness ).toEqual( 30 * SECOND );
 	} );
 
-	it( 'should imply defaults when no old requirements exist', () => {
-		const reqs = reduceItemRequirements( undefined, { freshness: 90 * SECOND } );
-		expect( reqs.timeout ).toEqual( DEFAULTS.timeout );
+	it( 'should set timeout if not previously set.', () => {
+		const endpointRequirements = {};
+		addRequirementParams( endpointRequirements, { timeout: 5 * SECOND } );
+		expect( endpointRequirements.timeout ).toEqual( 5 * SECOND );
 	} );
 
-	it( 'should update freshness to a smaller value', () => {
-		const freshness = DEFAULTS.freshness - 1000;
-		const reqs1 = { ...DEFAULTS, freshness };
-		const reqs2 = reduceItemRequirements( DEFAULTS, reqs1 );
-		expect( reqs2 ).not.toBe( DEFAULTS );
-		expect( reqs2.freshness ).toEqual( freshness );
+	it( 'should not change freshness if new value is higher.', () => {
+		const endpointRequirements = { freshness: 20 * SECOND };
+		addRequirementParams( endpointRequirements, { freshness: 30 * SECOND } );
+		expect( endpointRequirements.freshness ).toEqual( 20 * SECOND );
 	} );
 
-	it( 'should not update freshness to a larger value', () => {
-		const freshness = DEFAULTS.freshness - 10000;
-		const oldReqs = { ...DEFAULTS, freshness };
-		const reqs1 = { ...DEFAULTS, freshness: freshness + 1000 };
-		const reqs2 = reduceItemRequirements( oldReqs, reqs1 );
-		expect( reqs2 ).toBe( oldReqs );
+	it( 'should not change timeout if new value is higher.', () => {
+		const endpointRequirements = { timeout: 4 * SECOND };
+		addRequirementParams( endpointRequirements, { timeout: 5 * SECOND } );
+		expect( endpointRequirements.timeout ).toEqual( 4 * SECOND );
 	} );
 
-	it( 'should update timeout to a smaller value', () => {
-		const timeout = DEFAULTS.timeout - 1000;
-		const reqs1 = { ...DEFAULTS, timeout };
-		const reqs2 = reduceItemRequirements( DEFAULTS, reqs1 );
-		expect( reqs2 ).not.toBe( DEFAULTS );
-		expect( reqs2.timeout ).toEqual( timeout );
-	} );
-
-	it( 'should not update timeout to a larger value', () => {
-		const timeout = DEFAULTS.timeout + 10000;
-		const reqs1 = { ...DEFAULTS, timeout };
-		const reqs2 = reduceItemRequirements( DEFAULTS, reqs1 );
-		expect( reqs2 ).toBe( DEFAULTS );
+	it( 'should not change any other properties in the object', () => {
+		const endpointRequirements = {
+			freshness: 50 * SECOND,
+			timeout: 10 * SECOND,
+			endpoints: { 1: {}, 2: {} },
+			queries: [ { params: { a: 'a' } }, { params: { b: 'b' } } ],
+		};
+		addRequirementParams(
+			endpointRequirements,
+			{ freshness: 40 * SECOND, timeout: 5 * SECOND }
+		);
+		expect( endpointRequirements.endpoints ).toEqual( { 1: {}, 2: {} } );
+		expect( endpointRequirements.queries ).toEqual(
+			[ { params: { a: 'a' } }, { params: { b: 'b' } } ]
+		);
+		expect( endpointRequirements.freshness ).toEqual( 40 * SECOND );
+		expect( endpointRequirements.timeout ).toEqual( 5 * SECOND );
 	} );
 } );
 
-describe( 'reduceEndpointRequirements', () => {
-	it( 'should not update when nothing has changed.', () => {
-		const endpointReqs0 = {
-			1: { freshness: 30 * SECOND },
-			2: { freshness: 60 * SECOND },
-		};
-		const requirements = { freshness: 60 * SECOND };
-		const endpointReqs1 = reduceEndpointRequirements( endpointReqs0, [ 2 ], requirements );
-		expect( endpointReqs1 ).toBe( endpointReqs0 );
+describe( 'addEndpointRequirement', () => {
+	it( 'should add a requirement with no params, a single level deep', () => {
+		const reqs = {};
+		addEndpointRequirement( reqs, { freshness: 90 * SECOND }, [ 'thing' ] );
+		expect( reqs ).toEqual( {
+			thing: { freshness: 90 * SECOND, timeout: DEFAULTS.timeout },
+		} );
 	} );
 
-	it( 'should update an item when freshness is shorter', () => {
-		const endpointReqs0 = {
-			1: { freshness: 30 * SECOND },
-			2: { freshness: 60 * SECOND },
-		};
-		const requirements = { freshness: 45 * SECOND };
-		const endpointReqs1 = reduceEndpointRequirements( endpointReqs0, [ 2 ], requirements );
-		expect( endpointReqs1 ).not.toBe( endpointReqs0 );
-		expect( endpointReqs1[ 2 ].freshness ).toEqual( 45 * SECOND );
+	it( 'should add a requirement two levels deep', () => {
+		const reqs = {};
+		addEndpointRequirement(
+			reqs,
+			{ freshness: 10 * SECOND, timeout: 2 * SECOND },
+			[ 'thing', 2 ]
+		);
+		expect( reqs ).toEqual( {
+			thing: {
+				endpoints: {
+					2: { freshness: 10 * SECOND, timeout: 2 * SECOND },
+				},
+			},
+		} );
 	} );
 
-	it( 'should not update an item when freshness is longer', () => {
-		const endpointReqs0 = {
-			1: { freshness: 30 * SECOND },
-			2: { freshness: 60 * SECOND },
-		};
-		const requirements = { freshness: 70 * SECOND };
-		const endpointReqs1 = reduceEndpointRequirements( endpointReqs0, [ 2 ], requirements );
-		expect( endpointReqs1 ).toBe( endpointReqs0 );
+	it( 'should add requirements several levels deep', () => {
+		const reqs = {};
+		addEndpointRequirement(
+			reqs,
+			{ freshness: 20 * SECOND },
+			[ 'thing', 1, 'foot', 'red', 1 ]
+		);
+		addEndpointRequirement(
+			reqs,
+			{ freshness: 30 * SECOND },
+			[ 'thing', 2, 'foot', 'blue', 2 ]
+		);
+		expect( reqs ).toEqual( {
+			thing: {
+				endpoints: {
+					1: {
+						endpoints: {
+							foot: {
+								endpoints: {
+									red: {
+										endpoints: {
+											1: {
+												freshness: 20 * SECOND,
+												timeout: DEFAULTS.timeout
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+					2: {
+						endpoints: {
+							foot: {
+								endpoints: {
+									blue: {
+										endpoints: {
+											2: {
+												freshness: 30 * SECOND,
+												timeout: DEFAULTS.timeout
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		} );
 	} );
 
-	it( 'should update multiple items with the same freshness', () => {
-		const endpointReqs0 = {
-			1: { freshness: 50 * SECOND },
-			2: { freshness: 60 * SECOND },
-		};
-		const requirements = { freshness: 45 * SECOND };
-		const endpointReqs1 = reduceEndpointRequirements( endpointReqs0, [ 1, 2 ], requirements );
-		expect( endpointReqs1 ).not.toBe( endpointReqs0 );
-		expect( endpointReqs1[ 1 ].freshness ).toEqual( 45 * SECOND );
-		expect( endpointReqs1[ 2 ].freshness ).toEqual( 45 * SECOND );
+	it( 'should not change when adding a requirement that already exists', () => {
+		const reqs = {};
+		addEndpointRequirement( reqs, { freshness: 20 * SECOND }, [ 'thing' ] );
+		addEndpointRequirement( reqs, { freshness: 20 * SECOND }, [ 'thing' ] );
+		expect( reqs ).toEqual( {
+			thing: { freshness: 20 * SECOND, timeout: DEFAULTS.timeout },
+		} );
+	} );
+
+	it( 'should not change when adding a requirement that is superceded by another', () => {
+		const reqs = {};
+		addEndpointRequirement( reqs, { freshness: 20 * SECOND }, [ 'thing' ] );
+		addEndpointRequirement( reqs, { timeout: 3 * SECOND }, [ 'thing' ] );
+		addEndpointRequirement( reqs, { freshness: 60 * SECOND }, [ 'thing' ] );
+		addEndpointRequirement( reqs, { timeout: 5 * SECOND }, [ 'thing' ] );
+		expect( reqs ).toEqual( {
+			thing: { freshness: 20 * SECOND, timeout: 3 * SECOND },
+		} );
 	} );
 } );
 
+describe( 'combineComponentRequirements', () => {
+	it( 'should return empty if no components are in the map.', () => {
+		const reqsByEndpoint = combineComponentRequirements( new Map() );
+		expect( reqsByEndpoint ).toEqual( {} );
+	} );
+
+	it( 'should return endpoint requirements for a single component.', () => {
+		const reqsByComponent = new Map();
+		const component = () => null;
+		reqsByComponent.set( component, { freshness: 180 * SECOND, endpoint: [ 'thing', 2 ] } );
+
+		const reqsByEndpoint = combineComponentRequirements( reqsByComponent );
+		expect( reqsByEndpoint ).toEqual( {
+			thing: {
+				endpoints: {
+					2: { freshness: 180 * SECOND, timeout: DEFAULTS.timeout },
+				},
+			},
+		} );
+	} );
+
+	it( 'should return endpoint requirements for multiple components.', () => {
+		const reqsByComponent = new Map();
+		const component1 = () => null;
+		const component2 = () => null;
+		reqsByComponent.set( component1, { freshness: 60 * SECOND, endpoint: [ 'thing', 1 ] } );
+		reqsByComponent.set( component2, { freshness: 90 * SECOND, endpoint: [ 'thing', 2 ] } );
+
+		const reqsByEndpoint = combineComponentRequirements( reqsByComponent );
+		expect( reqsByEndpoint ).toEqual( {
+			thing: {
+				endpoints: {
+					1: { freshness: 60 * SECOND, timeout: DEFAULTS.timeout },
+					2: { freshness: 90 * SECOND, timeout: DEFAULTS.timeout },
+				},
+			},
+		} );
+	} );
+
+	it( 'should combine requirements for the same endpoint.', () => {
+		const reqsByComponent = new Map();
+		const component1 = () => null;
+		const component2 = () => null;
+		reqsByComponent.set( component1, { freshness: 30 * SECOND, endpoint: [ 'thing', 2 ] } );
+		reqsByComponent.set( component2, { freshness: 60 * SECOND, timeout: 2 * SECOND, endpoint: [ 'thing', 2 ] } );
+
+		const reqsByEndpoint = combineComponentRequirements( reqsByComponent );
+		expect( reqsByEndpoint ).toEqual( {
+			thing: {
+				endpoints: {
+					2: { freshness: 30 * SECOND, timeout: 2 * SECOND },
+				},
+			},
+		} );
+	} );
+} );
--- src/client/requirements.js
+++ src/client/requirements.js
@@ -5,26 +5,57 @@ export const DEFAULTS = {
 	timeout: 20 * SECOND,
 };
 
-export function reduceEndpointRequirements( endpointRequirements, ids, newRequirements ) {
-	return ids.reduce( ( requirements, id ) => {
-		const oldRequirements = endpointRequirements[ id ];
-		const itemRequirements = reduceItemRequirements( oldRequirements, newRequirements );
-		if ( itemRequirements !== oldRequirements ) {
-			return { ...requirements, [ id ]: itemRequirements };
-		}
-		return requirements;
-	}, endpointRequirements );
+/**
+ * Combines component requirements into a requirements list by endpoint.
+ * @param {Map} requirementsByComponent Key: Component, Value: requirements with endpoint/params property.
+ * @return {Object} New requirements endpoint tree.
+ */
+export function combineComponentRequirements( requirementsByComponent ) {
+	const requirements = {};
+
+	requirementsByComponent.forEach( ( requirement ) => {
+		const { endpoint, params, ...reqParams } = requirement;
+		addEndpointRequirement( requirements, reqParams, endpoint, params );
+	} );
+
+	return requirements;
 }
 
-export function reduceItemRequirements( oldRequirements = DEFAULTS, newRequirements ) {
-	const { freshness, timeout } = newRequirements;
-	let requirements = oldRequirements;
+/**
+ * Mutates the state of requirementsByEndpoint by adding a given endpoint requirement to it.
+ * @param {Object} requirementsByEndpoint Endpoint tree with requirement leaf nodes.
+ * @param {Object} reqParams Requirement parameters ( e.g. { freshness: 30 * SECOND } )
+ * @param {Array} endpointPath Array of strings representing endpoint path.
+ * @param {Object} params List of parameters for endpoint API call (optional).
+ */
+export function addEndpointRequirement( requirementsByEndpoint, reqParams, endpointPath, params ) {
+	const [ endpoint, ...remainingPath ] = endpointPath;
 
-	if ( freshness < oldRequirements.freshness ) {
-		requirements = { ...requirements, freshness };
+	if ( remainingPath.length === 0 ) {
+		const endpointRequirements = requirementsByEndpoint[ endpoint ] || { ...DEFAULTS };
+		addRequirementParams( endpointRequirements, reqParams );
+		requirementsByEndpoint[ endpoint ] = endpointRequirements;
+	} else {
+		const endpointRequirements = requirementsByEndpoint[ endpoint ] || {};
+		const endpoints = endpointRequirements.endpoints || {};
+		addEndpointRequirement( endpoints, reqParams, remainingPath, params );
+
+		endpointRequirements.endpoints = endpoints;
+		requirementsByEndpoint[ endpoint ] = endpointRequirements;
 	}
-	if ( timeout < oldRequirements.timeout ) {
-		requirements = { ...requirements, timeout };
-	}
-	return requirements;
+}
+
+/**
+ * Merges new requirement parameters into existing ones.
+ * @param {Object} endpointRequirements Contains requirement parameters, possibly endpoints, and queries.
+ * @param {Object} reqParams New requirement parameters (freshness, timeout), to be merged with existing ones.
+ */
+export function addRequirementParams( endpointRequirements, reqParams ) {
+	const freshness = endpointRequirements.freshness || DEFAULTS.freshness;
+	const timeout = endpointRequirements.timeout || DEFAULTS.timeout;
+	const newFreshness = reqParams.freshness || Number.MAX_SAFE_INTEGER;
+	const newTimeout = reqParams.timeout || Number.MAX_SAFE_INTEGER;
+
+	endpointRequirements.freshness = Math.min( freshness, newFreshness );
+	endpointRequirements.timeout = Math.min( timeout, newTimeout );
 }
