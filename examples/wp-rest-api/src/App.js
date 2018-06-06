cee559caa8fdd5a9bea57f1e3c89d800d49b821d
Kevin Killingsworth <kevin@redfenix.com>
2018-06-06 17:03:44 -0500

    Fix wp-api url.
    It doesn't support https

--- examples/wp-rest-api/src/App.js
+++ examples/wp-rest-api/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
 				<h1 className="App-title">Fresh Data</h1>
 			</header>
 			<header className="WP-header">
-				<h2><a className="WP-title" href="https://wp-api.org">WordPress REST API</a></h2>
+				<h2><a className="WP-title" href="http://wp-api.org">WordPress REST API</a></h2>
 			</header>
 			<p className="App-intro">
 				TODO: Add components here.
