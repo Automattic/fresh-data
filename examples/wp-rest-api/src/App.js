4050a586b25a8f1cb1b58d4cfe1188f71b85f1a2
Kevin Killingsworth <kevin@redfenix.com>
2018-06-06 00:45:09 -0500

    examples/wp-rest-api: update default page
    Updates the html, css, js to mention Fresh Data, WordPress REST API,
    and a link to the API docs. Also removes the service worker as it
    doesn't apply to example projects like this.

--- examples/wp-rest-api/src/App.js
+++ examples/wp-rest-api/src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 className="App-title">Fresh Data</h1>
+			</header>
+			<header className="WP-header">
+				<h2><a className="WP-title" href="https://wp-api.org">WordPress REST API</a></h2>
+			</header>
+			<p className="App-intro">
+				TODO: Add components here.
+			</p>
+		</div>
+	);
+};
 
 export default App;
--- examples/wp-rest-api/src/index.js
+++ examples/wp-rest-api/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render( <App />, document.getElementById( 'root' ) );
