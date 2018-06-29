979c39482f5b867b8cc1759752d8b77d0901a1b9
Kevin Killingsworth <kevin@redfenix.com>
2018-06-29 09:53:44 -0600

    resourceName: Remove endpointPath/params from actions
    This removes the endpointPath/params from the data handling actions as
    they're no longer needed.

--- src/react-redux/__tests__/actions.spec.js
+++ src/react-redux/__tests__/actions.spec.js
@@ -13,70 +13,62 @@ describe( 'actions', () => {
 	const apiName = 'myApiName';
 	const clientKey = 'myClientKey';
 	const resourceName = 'one-two-three:{ param1: "one", param2: "two" }';
-	const endpointPath = [ 'one', 'two', 3 ];
-	const params = { param1: 'one', param2: 2 };
 	const data = { attributes: { attr1: '1', attr2: '2', attr3: 3 } };
 	const error = { message: 'oops!' };
 	const time = new Date();
 
 	describe( 'dataRequested', () => {
 		it( 'should create a simple action object with expected fields.', () => {
-			const action = dataRequested( apiName, clientKey, resourceName, endpointPath, params, time );
+			const action = dataRequested( apiName, clientKey, resourceName, time );
 			expect( action ).toEqual( {
 				type: FRESH_DATA_CLIENT_REQUESTED,
 				apiName,
 				clientKey,
 				resourceName,
-				endpointPath,
-				params,
 				time,
 			} );
 		} );
 
 		it( 'should set a default time if none given.', () => {
-			const action = dataRequested( apiName, clientKey, resourceName, endpointPath, params );
+			const action = dataRequested( apiName, clientKey, resourceName );
 			expect( action.time ).toBeInstanceOf( Date );
 		} );
 	} );
 
 	describe( 'dataReceived', () => {
 		it( 'should create a simple action object with expected fields.', () => {
-			const action = dataReceived( apiName, clientKey, resourceName, endpointPath, params, data, time );
+			const action = dataReceived( apiName, clientKey, resourceName, data, time );
 			expect( action ).toEqual( {
 				type: FRESH_DATA_CLIENT_RECEIVED,
 				apiName,
 				clientKey,
 				resourceName,
-				endpointPath,
-				params,
 				data,
 				time,
 			} );
 		} );
 
 		it( 'should set a default time if none given.', () => {
-			const action = dataReceived( apiName, clientKey, resourceName, endpointPath, params, data );
+			const action = dataReceived( apiName, clientKey, resourceName, data );
 			expect( action.time ).toBeInstanceOf( Date );
 		} );
 	} );
 
 	describe( 'errorReceived', () => {
 		it( 'should create a simple action object with expected fields.', () => {
-			const action = errorReceived( apiName, clientKey, resourceName, endpointPath, params, error, time );
+			const action = errorReceived( apiName, clientKey, resourceName, error, time );
 			expect( action ).toEqual( {
 				type: FRESH_DATA_CLIENT_ERROR,
 				apiName,
 				clientKey,
 				resourceName,
-				endpointPath,
-				params,
 				error,
 				time,
 			} );
 		} );
 
 		it( 'should set a default time if none given.', () => {
-			const action = errorReceived( apiName, clientKey, resourceName, endpointPath, params, error );
+			const action = errorReceived( apiName, clientKey, resourceName, error );
 			expect( action.time ).toBeInstanceOf( Date );
 		} );
 	} );
--- src/react-redux/actions.js
+++ src/react-redux/actions.js
@@ -4,39 +4,33 @@ import {
 	FRESH_DATA_CLIENT_REQUESTED,
 } from './action-types';
 
-export function dataRequested( apiName, clientKey, resourceName, endpointPath, params, time = new Date() ) {
+export function dataRequested( apiName, clientKey, resourceName, time = new Date() ) {
 	return {
 		type: FRESH_DATA_CLIENT_REQUESTED,
 		apiName,
 		clientKey,
 		resourceName,
-		endpointPath,
-		params,
 		time,
 	};
 }
 
-export function dataReceived( apiName, clientKey, resourceName, endpointPath, params, data, time = new Date() ) {
+export function dataReceived( apiName, clientKey, resourceName, data, time = new Date() ) {
 	return {
 		type: FRESH_DATA_CLIENT_RECEIVED,
 		apiName,
 		clientKey,
 		resourceName,
-		endpointPath,
-		params,
 		data,
 		time,
 	};
 }
 
-export function errorReceived( apiName, clientKey, resourceName, endpointPath, params, error, time = new Date() ) {
+export function errorReceived( apiName, clientKey, resourceName, error, time = new Date() ) {
 	return {
 		type: FRESH_DATA_CLIENT_ERROR,
 		apiName,
 		clientKey,
 		resourceName,
-		endpointPath,
-		params,
 		error,
 		time,
 	};
