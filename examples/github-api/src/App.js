26058856129c318b8c16797aad3edfa6ff372c83
Kevin Killingsworth <kevin@redfenix.com>
2018-09-10 00:37:37 -0500

    github-example: Clear out defaults
    This clears out the default create-react-app and creates a clean slate
    from which to work.

--- examples/github-api/src/App.js
+++ examples/github-api/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Fresh Data</h1>
+				</header>
+				<header className="GH-header">
+					<h2><a className="GH-title" href="https://developer.github.com/v3">GitHub REST API</a></h2>
+				</header>
+				<p>
+					Hello!
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
