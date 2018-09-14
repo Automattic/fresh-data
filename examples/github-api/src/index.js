8fef7e93304c4f3e7b694cc796b5cb936915547e
Kevin Killingsworth <kevin@redfenix.com>
2018-09-14 00:00:03 -0500

    github-example: Add redux store

--- examples/github-api/src/index.js
+++ examples/github-api/src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import createStore from './create-store';
+import reducer from './reducer';
+import 'babel-polyfill';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+const store = createStore( reducer );
+
+ReactDOM.render( <App store={ store } />, document.getElementById( 'root' ) );
