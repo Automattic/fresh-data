42fb16540c8f9e5563e36104a470160f311e9270
Kevin Killingsworth <kevin@redfenix.com>
2019-04-29 10:15:15 -0500

    wp-rest-api: use reducer from framework instead.

--- examples/wp-rest-api/src/reducer.js
+++ examples/wp-rest-api/src/reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { reducer as freshData } from '@fresh-data/react-provider';
+import { reducer as freshData } from '@fresh-data/framework';
 
 const reducers = {
 	freshData,
