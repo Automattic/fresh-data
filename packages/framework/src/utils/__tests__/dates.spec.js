033283ad4c72f62406f870e161d8c60dab6b01d6
Kevin Killingsworth <kevin@redfenix.com>
2019-05-10 14:00:22 -0500

    Add isDateEarlier utility function.
    This adds a utility function: isDateEarlier and tests for it.

---
+++ packages/framework/src/utils/__tests__/dates.spec.js
@@ -0,0 +1,30 @@
+import { isDateEarlier } from '../dates';
+
+describe( 'isDateEarlier', () => {
+	it( 'returns false an exact match', () => {
+		const now = new Date();
+		expect( isDateEarlier( now, now ) ).toBe( false );
+	} );
+
+	it( 'returns true if no existing date', () => {
+		const now = new Date();
+		expect( isDateEarlier( null, now ) ).toBe( true );
+	} );
+
+	it( 'returns false if no new date', () => {
+		const now = new Date();
+		expect( isDateEarlier( now, null ) ).toBe( false );
+	} );
+
+	it( 'returns true for one millisecond earlier', () => {
+		const now = new Date();
+		const earlier = new Date( now.getTime() - 1 );
+		expect( isDateEarlier( now, earlier ) ).toBe( true );
+	} );
+
+	it( 'returns false for one millisecond later', () => {
+		const now = new Date();
+		const earlier = new Date( now.getTime() + 1 );
+		expect( isDateEarlier( now, earlier ) ).toBe( false );
+	} );
+} );
